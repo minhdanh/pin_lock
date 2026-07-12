8b68ead86c7ab7cb876b69af22ffd1c7c8f8feec
minhdanh <minhdanh@users.noreply.github.com>
2026-07-12 17:54:23 +0700

    chore: update JVM target to 17 and upgrade local_auth to v3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,7 @@ Future<void> main() async {
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
